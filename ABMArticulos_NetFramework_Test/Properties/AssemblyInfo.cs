d20773faf02536c5fe9bb56710f6148113cbf05b
Oscar Centurion <oscarcenturion@timesolution.com.ar>
2023-06-11 03:33:15 -0300

    se agregan pruebas unitarias para ControladorArticulos.

---
+++ ABMArticulos_NetFramework_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ABMArticulos_NetFramework_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("ABMArticulos_NetFramework_Test")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fbf5b274-fa1e-4406-9b06-698e4438a1f1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ABMArticulos_NetFramework_Test/TestControladores/TestControladorArticulos.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ABMArticulos_NetFramework_Test
+{
+    [TestClass]
+    public class TestControladorArticulos
+    {
+        [TestMethod]
+        public void InsertArticulo_DeberiaInsertarNuevoArticulo()
+        {
+            // Arrange
+            var articuloService = new ABMArticulos_NetFramework_API.Controladores.ControladorArticulos();
+            var descripcion = "Descripción del artículo";
+
+            // Act
+            var id = articuloService.InsertArticulo(descripcion);
+
+            // Assert
+            Assert.IsTrue(id > 0);
+        }
+    }
+}
